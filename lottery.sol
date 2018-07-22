448b2a23cd6cb0cae5c6834a472ee3904d97e9af
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-07-22 15:43:35 -0400

    insert/modify options for the lottery
    added ability to insert/modify options for the lottery

--- lottery.sol
+++ lottery.sol
@@ -24,7 +24,6 @@ import "./safeMath.sol";
     uint public lastParticipator;                                    // keep track of total participants
     uint public totWinners;                                          // total no.of lottery winners!
     uint public profitAmt;                                           // (total pot)/(total winners)
-    uint[] public choices;                                           // options from which participants can choose
     uint public buyIn;                                               // minimum amount to be eligible to participate
     uint public participationFee;                                    // owners cut
     
@@ -40,8 +39,10 @@ import "./safeMath.sol";
     mapping (uint => Participator) participatorInfo;
     mapping (address => uint) addrToID;
     mapping (address => bool) alreadyApproved;
+    mapping (uint => uint) public options;                           // options from which participants can choose to place bet
     
     // ------------------ Modifiers -----------------\\
+    
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
@@ -78,29 +79,27 @@ import "./safeMath.sol";
         lastParticipator = 0;
         totWinners = 0;
         profitAmt = 0;
-        choices = [1,2,3];
         buyIn = 0.5 ether;
         participationFee = 0.0001 ether;
+        options[1] = 1;
+        options[2] = 2;
+        options[3] = 3;
     }
 
     // ------------------ Public/External functions -----------------\\
-    
-    function placeBet(uint _choice) public lotteryOnGoing payable{
+
+    function placeBet(uint _playerChoice) public lotteryOnGoing payable{
     // Function to place bet (choose an option)
         require(msg.value >= buyIn.add(participationFee), "Amount < (Buy-in + Participation fee)");
         require(alreadyApproved[msg.sender] != true, "You've already placed your bet!");
 
         lastParticipator = lastParticipator.add(1);                             // using safemath syntax
         uint _participatorNo = lastParticipator;
-        if (_choice == choices[0]){
-            participatorInfo[_participatorNo].choice = choices[0];
-        } else if (_choice == choices[1]){
-            participatorInfo[_participatorNo].choice = choices[1];
-        }else if (_choice == choices[2]){
-            participatorInfo[_participatorNo].choice = choices[2];
+        if (options[_playerChoice] != 0){
+            participatorInfo[_participatorNo].choice = options[_playerChoice];
         }else{
             lastParticipator = lastParticipator.sub(1);                         // using safemath syntax
-            require(1==2, "Choose valid option!");                              // Throw error
+            require(false, "Choose valid option!");                             // Throw error
         }
 
         participatorInfo[_participatorNo].sender = msg.sender;
@@ -130,7 +129,7 @@ import "./safeMath.sol";
         uint _userID = addrToID[msg.sender];
         require(!participatorInfo[_userID].profitReceived, "You've already received the profits!");
         if (participatorInfo[_userID].choice == winningChoice){
-            if (participatorInfo[_userID].bettingTime < timeToDraw){         // Dont disburse profits to those who made bet after timeToDraw
+            if (participatorInfo[_userID].bettingTime < timeToDraw){            // Dont disburse profits to those who made bet after timeToDraw
                 (msg.sender).transfer(profitAmt);
                 participatorInfo[_userID].profitReceived = true;
                 alreadyApproved[msg.sender] = false;
@@ -167,7 +166,7 @@ import "./safeMath.sol";
     // Internal function to calculate the profits each winner will receive!
         require(winningChoice != 0, "Owner has not yet set the 'winning choice'!");
         for (uint i=1; i<lastParticipator+1; i++){
-            if (participatorInfo[i].bettingTime < timeToDraw){               // Dont include users who made bet after timeToDraw
+            if (participatorInfo[i].bettingTime < timeToDraw){                  // Dont include users who made bet after timeToDraw
                 if (participatorInfo[i].choice == winningChoice){
                     totWinners = totWinners.add(1);                             // using safemath syntax
                 }
@@ -179,10 +178,6 @@ import "./safeMath.sol";
     function currentTime() view external onlyOwner returns(uint){
     // Helper function to set timeToDraw
         return now;
-    }
-
-    function getParticipatorInfo(uint id) view external onlyOwner returns (address senderAddr, uint choice, uint timeOfBet, bool profitReceived){
-        return (participatorInfo[id].sender, participatorInfo[id].choice, participatorInfo[id].bettingTime, participatorInfo[id].profitReceived);
     }
 
     function setWiningChoice(uint _randomChoice) external onlyOwner encashDurationOngoing{
@@ -215,6 +210,15 @@ import "./safeMath.sol";
         totWinners = 0;
         lastParticipator = 0;
         profitAmt = 0;
+    }
+
+    function setOptions(uint[] _array) external onlyOwner lotteryEnded{
+    // Function to add or remove options
+        for (uint i=0;i<_array.length-1;i+2){
+            uint key = _array[i];
+            uint val = _array[i++];
+            options[key] = val;
+        }
     }
 
     function setEncashDuration(uint _newDuration) external onlyOwner encashDurationOngoing{
